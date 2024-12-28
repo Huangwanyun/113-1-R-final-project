64daf095dc2464f08004836091b8083bf019d978
econmtlin <g13425526@gmail.com>
2024-12-28 20:08:44 +0000

    1228

--- 1228.R
+++ 1228.R
@@ -24,4 +24,27 @@ dim(data)
 plot(data$Column1, data$Column2, main="Scatter Plot", xlab="Column1", ylab="Column2")
 
 library(dplyr)
+filtered_data <- data %>%
+  filter(學校名稱 == "國立臺北大學")
+
+#
+NTPU_data <- data.frame(
+  印尼僑生 = c(38),  # number of people
+  寮國僑生 = c(0),
+  新加坡僑生 = c(2),
+  柬埔寨僑生 = c(0),
+  汶萊僑生 = c(1),
+  泰國僑生 = c(6),
+  緬甸僑生 = c(86),
+  菲律賓僑生 = c(1),
+  越南僑生 = c(17),
+  馬來西亞僑生 = c(116)
+)
+
+data <- c(38, 0, 2, 0, 1, 6, 86, 1, 17, 116)
+class(data)
+countries <- c("印尼僑生", "寮國僑生", "新加坡僑生", "柬埔寨僑生", "汶萊僑生", 
+               "泰國僑生", "緬甸僑生", "菲律賓僑生", "越南僑生", "馬來西亞僑生")
+class(countries)
+
 
